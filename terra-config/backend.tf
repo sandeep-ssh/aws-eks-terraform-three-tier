7efc31909235becdbb4aa34f503d03dd27d07556
sandeep-ssh <sashcell@yahoo.co.in>
2025-12-30 15:49:56 +1100

    Final clean up of files for project

--- terra-config/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "sandeep-eks-terraform-three-tier"  # Change if the name already exists. 
-    key            = "eks/terraform.tfstate"       
-    region         = "us-east-1"                   
-    encrypt        = true
-  }
-}
--- terra-config/ecr.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecrpublic_repository" "frontend" {
-  repository_name  = "three-tier-frontend"
-}
-
-resource "aws_ecrpublic_repository" "backend" {
-  repository_name  = "three-tier-backend"
-  
-}
